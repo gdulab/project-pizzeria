06636c8f2d9ca2a2168fad59a1ed40395e18e976
gdulab <gdula.bartlomiej@gmail.com>
2023-04-12 18:42:29 +0200

    ADD Accordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,11 +60,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
-
-      console.log('new Product: ', thisProduct);
+      thisProduct.initAccordion();
     }
 
-    renderInMenu(){
+    renderInMenu() {
       const thisProduct = this;
 
       /* generate HTML based on template */
@@ -76,6 +75,29 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+
+        if (thisProduct.element != activeProduct) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        
+      });
+    }
   }
 
 
@@ -84,7 +106,7 @@
     initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data: ', thisApp.data);
-      for (let productData in thisApp.data.products){
+      for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
